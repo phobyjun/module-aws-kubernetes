d6cbf6b7eceb5604a28d38b58d62270f1973b4cf
phobyjun <phobyjun@gmail.com>
2022-01-29 23:31:26 +0900

    kubernetes module complete

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "eks_cluster_id" {
+  value = aws_eks_cluster.js-eks-cluster.id
+}
+
+output "eks_cluster_name" {
+  value = aws_eks_cluster.js-eks-cluster.name
+}
+
+output "eks_cluster_certificate_date" {
+  value = aws_eks_cluster.js-eks-cluster.certificate_authority.0.data
+}
+
+output "eks_cluster_endpoint" {
+  value = aws_eks_cluster.js-eks-cluster.endpoint
+}
+
+output "eks_cluster_nodegroup_id" {
+  value = aws_eks_node_group.js-node-group.id
+}
